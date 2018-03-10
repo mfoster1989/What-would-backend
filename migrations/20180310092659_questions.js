45dc25a16392ebdfe32097e89d295f6d7a99f27a
Malcolm Foster <malcolmfoster@Malcolms-MacBook-Air.local>
2018-03-10 09:40:46 -0700

    seeds and migrations

--- migrations/20180310092659_questions.js
+++ migrations/20180310092659_questions.js
@@ -2,11 +2,11 @@ exports.up = function (knex, Promise) {
     return knex.schema.createTable("questions", function (table) {
         table.increments('id').primary()
         table.text('title')
-        table.string('question')
+        table.text('question')
         table.text('answer1')
         table.text('answer2')
         table.integer('response1')
-        table.integer('response1')
+        table.integer('response2')
     })
 };
 
--- seeds/01_questions.js
+++ seeds/01_questions.js
@@ -5,9 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {id: 1, title: "", question: "", question2: "", answer1: "", answer2: "", response1: "", response2: ""},
       ]);
     });
 };
